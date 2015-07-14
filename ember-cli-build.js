3cda4a1abc8dd0659427d8e59a686ccea2263d62
MX Chai <mingxuan@live.com.sg>
2015-07-14 10:25:47 +0800

    Installed Materialize.css as bower_component. Added to ember-cli-build.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,5 +19,8 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('bower_components/materialize/dist/css/materialize.css');
+  app.import('bower_components/materialize/dist/js/materialize.js');
+
   return app.toTree();
 };
