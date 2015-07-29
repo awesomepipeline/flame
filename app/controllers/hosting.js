86fe3a76916360f416dc450743b92f48ba3cd072
MX Chai <mingxuan@live.com.sg>
2015-07-29 20:43:09 +0800

    Fixed wrong session key fetching attribute

--- app/controllers/hosting.js
+++ app/controllers/hosting.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  sessionKey: Ember.$.parseJSON(localStorage["ember_simple_auth:session"])["secure"]["auth_token"]
+  sessionKey: Ember.$.parseJSON(localStorage["ember_simple_auth:session"])["secure"]["token"]
 });
