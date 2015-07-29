e69bbf8ab9f42eb799c2c6ea6dd0f26e79763249
MX Chai <mingxuan@live.com.sg>
2015-07-29 20:05:59 +0800

    managed to extract localstorage session key

--- app/routes/pending.js
+++ app/routes/pending.js
@@ -1,4 +1,8 @@
 import AuthRoute from './auth-route';
+import Base from 'simple-auth/stores/base';
 
 export default AuthRoute.extend({
+  beforeModel: function() {
+    console.log(localStorage);
+  }
 });
