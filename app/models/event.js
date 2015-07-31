bab144c07c541f611b50ed7807e47242ca01a2f9
MX Chai <mingxuan@live.com.sg>
2015-07-31 22:10:01 +0800

    Events list now show event details

--- app/models/event.js
+++ app/models/event.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   activity: DS.attr('string'),
   datetime: DS.attr('date'),
+  description: DS.attr('string'),
   host: DS.attr('number'),
   created_at: DS.attr('date')
 });
