0c3f2d4f08c0fd5833ed18e7e8d8ce49f49f2de6
MX Chai <mingxuan@live.com.sg>
2015-08-05 00:27:17 +0800

    Set default datetime for event creation (easier to debug)

--- app/models/event.js
+++ app/models/event.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   activity: DS.attr('string'),
-  datetime: DS.attr('string'),
+  datetime: DS.attr('string', {defaultValue: '2015-08-30T00:00:00.000+08:00'}),
   location: DS.attr('string'),
   description: DS.attr('string'),
   host: DS.attr('number'),
