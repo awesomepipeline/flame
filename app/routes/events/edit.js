78f40f2a5085a343ec709e0699122f84319e41c5
MX Chai <mingxuan@live.com.sg>
2015-08-04 17:12:20 +0800

    Edit event now works

--- app/routes/events/edit.js
+++ app/routes/events/edit.js
@@ -4,4 +4,18 @@ export default Ember.Route.extend({
   // model(params) {
   //   return this.store.findAll('event', params.event_id);
   // }
+  setupController: function(controller, model) {
+    controller.set('event', model);
+  },
+
+  actions: {
+    updateEvent: function(_event) {
+      console.log("Edit action: this is bubbled to me");
+      var _this = this;
+      _event.save().then(function(_event) {
+        console.log(_event);
+        _this.transitionTo('events.show', _event);
+      });
+    }
+  }
 });
--- app/serializers/event.js
+++ app/serializers/event.js
@@ -2,9 +2,10 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
   // Holy cow I got this to work -.-
-  // Ember data expects a root object, instead of raw JSON
-  // Hence {event: [{response}]} instead of just {response}
+  // Ember data expects a ROOT OBJECT, instead of raw JSON
+  // Hence {event: [{response}]} instead of just {response}, which is what server is sending back
   extractSingle: function(store, typeClass, payload, id) {
+    // rooting the payload in event
     payload = { event: [payload] };
     return this._super(store, typeClass, payload, id);
   }
