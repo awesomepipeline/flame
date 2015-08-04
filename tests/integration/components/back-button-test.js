a04fe750d5ec59888709390a5b28cb20335ddbc4
MX Chai <mingxuan@live.com.sg>
2015-08-04 22:27:49 +0800

    Implemented back button to hosting

---
+++ tests/integration/components/back-button-test.js
@@ -0,0 +1,27 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+
+moduleForComponent('back-button', 'Integration | Component | back button', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{back-button}}`);
+
+  assert.equal(this.$().text(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#back-button}}
+      template block text
+    {{/back-button}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
