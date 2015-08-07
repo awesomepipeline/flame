1fd953add8ea12c0f10003232c2dca6af25f035c
MX Chai <mingxuan@live.com.sg>
2015-08-07 18:58:09 +0800

    Can now accept and decline events! Shows a toast (little popup) when you perform either action

--- app/routes/invitations/index.js
+++ app/routes/invitations/index.js
@@ -11,5 +11,31 @@ export default AuthRoute.extend({
     };
 
     return Ember.$.ajax(settings);
+  },
+
+  actions: {
+    decline: function(invitationId) {
+      Materialize.toast("Event declined :(", 2000);
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:3000/api/v1/events/" + invitationId + "/reject",
+        "method": "GET",
+      };
+
+      Ember.$.ajax(settings);
+    },
+
+    accept: function(invitationId) {
+      Materialize.toast("Event accepted :)", 2000);
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:3000/api/v1/events/" + invitationId + "/accept",
+        "method": "GET",
+      };
+
+      Ember.$.ajax(settings);
+    }
   }
 });
