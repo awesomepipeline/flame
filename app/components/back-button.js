6b84d9b45981bcf6c1da346613361717333b5c71
MX Chai <mingxuan@live.com.sg>
2015-08-08 00:32:44 +0800

    Return of the overloaded Back Button!

--- app/components/back-button.js
+++ app/components/back-button.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  buttonLabel: function() {
+    if (this.get('type') === "events") {
+      return "Back to Events";
+    } else {
+      return "Back to Invitations";
+    }
+  }.property(),
+
+  isEventsRoute: function() {
+    if (this.get('type') === 'events') {
+      return true;
+    } else {
+      return false;
+    }
+  }.property(),
+
+  clown: "hello"
 });
