7db1e8be0f6666663ea41aee889ce749553e4767
MX Chai <mingxuan@live.com.sg>
2015-08-07 16:17:11 +0800

    Invitation details now fetches the invitation details (people who've accepted / rejected). Passes in this information to the invitation-card-details component now

--- app/routes/invitations/show.js
+++ app/routes/invitations/show.js
@@ -3,25 +3,26 @@ import AuthRoute from '../auth-route';
 
 export default AuthRoute.extend({
   model: function(params) {
+    var _this = this;
+
     var settings = {
       "async": true,
       "crossDomain": true,
       "url": "http://localhost:3000/api/v1/user/events/" + params.invitation_id,
       "method": "GET",
+    };   
+
+    var invitationSettings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "http://localhost:3000/api/v1/events/" + params.invitation_id + "/responses",
+      "method": "GET",
     };
-    return Ember.$.ajax(settings);
+
+    return Ember.RSVP.hash({
+      eventDetails: Ember.$.ajax(settings),
+      invitationDetails: Ember.$.ajax(invitationSettings)
+    });
   },
-
-  deactivate: function() {
-    // var currentModel = this.modelFor('events.show');
-    // var currentUser = this.get('session.content.secure.username');
-
-    // console.log(currentModel);
-    // // console.log(currentModel.get('host').username);
-    // console.log(this.get('session.content.secure.username'));
-
-
-    // this.store.deleteRecord(this.modelFor('events.show'));
-  }
 });
 
