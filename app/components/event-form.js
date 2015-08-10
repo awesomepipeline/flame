300873c9905f60729fd0fc2a4e3db45d39c566f8
MX Chai <mingxuan@live.com.sg>
2015-08-10 21:28:37 +0800

    Annontated code

--- app/components/event-form.js
+++ app/components/event-form.js
@@ -3,13 +3,14 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   buttonLabel: function() {
     return (this.get('event').id) ? 'Update Event' : 'Create Event';
-  }.property(),
-
-  
+  }.property(),  
 
   actions: {
     submit() {
       // Single event, hence this.sendAction('action', ...)
+      // this.get('event') gets all the filled in stuff, because in the .hbs
+      // the input values are populated with event.property. There's data
+      // binding going on that's why.
       this.sendAction('action', this.get('event'));
     }
   }
