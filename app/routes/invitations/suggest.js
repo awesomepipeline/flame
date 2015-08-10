153ff4b9ab3bb36d2ac6630fa6949d928a2e0782
MX Chai <mingxuan@live.com.sg>
2015-08-10 21:35:59 +0800

    Updated with setupController

--- app/routes/invitations/suggest.js
+++ app/routes/invitations/suggest.js
@@ -1,4 +1,5 @@
 import AuthRoute from 'flame/routes/auth-route';
+import DS from 'ember-data';
 import Ember from 'ember';
 import InvitationsAdapter from 'flame/adapter/invitations';
 
@@ -10,17 +11,31 @@ export default AuthRoute.extend({
     return this.get('adapter').suggestModel(params.invitation_id);
   },
 
+  setupController: function(controller, model) {
+    controller.set('event', model);
+    controller.set('errors', DS.Errors.create());
+  },
+
   actions: {
     submitSuggestion: function(type, value, invitationId) {
       console.log(type);
-      console.log(value);
+      console.log(value.length);
+      console.log(this.get('controller'));
+
+      // Client side validation for this 
+      if (value.length === 0) {
+
+      }
 
       if (type === "activity") {
         // Send the activity request
+        this.get('adapter').suggestActivity();
       } else if(type === "time") {
         // Send the time request
+        this.get('adapter').suggestTime();
       } else if (type === "location") {
         // Send the location request
+        this.get('adapter').suggestLocation();
       } else {
         // 
       }
