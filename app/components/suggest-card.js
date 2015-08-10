568f3848a2662f1cc1934fa10ed66a22b4acd292
MX Chai <mingxuan@live.com.sg>
2015-08-10 21:54:18 +0800

    Client side validation set up for suggestion

--- app/components/suggest-card.js
+++ app/components/suggest-card.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   actions: {
     submitSuggestion: function(invitation) {
       var dropdownSelection = $('#dropdown').val();
-      var suggestionText = $('suggestionText').val();
+      var suggestionText = $('#suggestionText').val();
       this.sendAction('action', dropdownSelection, suggestionText, this.get('card-data').id);
     }
   }
