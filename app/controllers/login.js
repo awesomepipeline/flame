c5cedcb9e2bb321131f112042988c5ae78190881
MX Chai <mingxuan@live.com.sg>
2015-08-11 11:36:06 +0800

    Error handling in place for login

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,5 +2,22 @@ import Ember from 'ember';
 import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
 
 export default Ember.Controller.extend(LoginControllerMixin, {
-  authenticator: 'simple-auth-authenticator:devise'
+  authenticator: 'simple-auth-authenticator:devise',
+  
+  // Just a simple error property, there's no need to use DS.Errors because 
+  // the error message is not an array. It's just a message
+  error: '',
+
+  actions: {
+    authenticate: function() {
+      // identification and password are the defauls for ember simple auth
+      var data = this.getProperties('identification', 'password');
+      var _this = this;
+
+      this.get('session').authenticate(this.get('authenticator'), data)
+        .fail(function(res) {
+          _this.set('error', res.error);
+        });
+    }
+  }
 });
